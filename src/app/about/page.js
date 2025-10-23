33372bfc474af77c4b1a710a2f6e38a32fdd9df5
Paul Neenan <paulneenan@Mac.lan>
2025-10-23 15:39:11 -0400

    fixes import

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,5 @@
 // app/about/page.js
-import AboutContent from "@/components/AboutContent";
+import AboutContent from "@components/AboutContent";
 import styles from "./page.module.css";
 
 export default function AboutPage() {
